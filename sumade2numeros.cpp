4a4b9e69972c5ebe73a32f8520c3be2d46b1db5b
juan <juanjo_070193@hotmail.com>
2013-02-11 09:17:54 -0600

    escaneo de numero a y b

--- sumade2numeros.cpp
+++ sumade2numeros.cpp
@@ -4,7 +4,13 @@
 int main()
 {
 	int a, b,c;
-	
+	printf("Dame el primer numero ");
+	scanf("%d",&a);
+	printf("Dame el segundo numero ");
+	scanf("%d",&b);
+	c=a+b;
+	system("cls");
+	printf("El resultado de la suma es:%d",c);
 	
 	
 	getchar();
